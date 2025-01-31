d746c2ff9b2bfe53c7563a1468ab87713059bc6f
samin1994 <saminlabbaf1994@gmail.com>
2025-01-31 01:53:13 -0500

    added skills section and education and work experience

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BiDownload } from "react-icons/bi";
 import { FaGithub } from "react-icons/fa6";
 import { GrLinkedinOption } from "react-icons/gr";
 import { GoArrowUpRight } from 'react-icons/go';
+import { LuMedal } from "react-icons/lu";
+import { PiGraduationCap } from "react-icons/pi";
 
 export default function Home() {
 
@@ -32,8 +34,41 @@ export default function Home() {
     },
   ];
 
+  const projects = [
+    {
+      title: "Dr Safdarian Clinic",
+      description: "This is a description of project 1",
+      link: "https://example.com/project1",
+      image: "/img/dermatology_clinic.jpeg",
+      category: "code",
+    },
+    {
+      title: "Climate App Design",
+      description: "This is a description of project 2",
+      link: "https://example.com/project2",
+      image: "/img/climate_app.png",
+      category: "design",
+    }
+  ]
 
+  const [selectedCategory, setSelectedCategory] = useState('all')
+  const [filteredProjects, setFilteredProjects] = useState([])
 
+  useEffect(() => {
+    // filter projects based on selected category
+    if (selectedCategory === 'all') {
+      setFilteredProjects(projects)
+    } else if (selectedCategory === 'code') {
+      setFilteredProjects(projects.filter(project => project.category === 'code'))
+    }
+    else if (selectedCategory === 'design') {
+      setFilteredProjects(projects.filter(project => project.category === 'design'))
+    }
+  }, [selectedCategory])
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category)
+  }
 
   return (
     <>
@@ -109,41 +144,149 @@ export default function Home() {
             <p>A collection of projects featuring my work in both web design and development, with separate categories for design and responsive development.</p>
           </div>
           <div className="project_buttons">
-            <button>All</button>
-            <button>Code</button>
-            <button>Design</button>
+            <button className={selectedCategory === 'all' ? 'active' : ''} onClick={() => setSelectedCategory('all')}>All</button>
+            <button className={selectedCategory === 'code' ? 'active' : ''} onClick={() => setSelectedCategory('code')}>Code</button>
+            <button className={selectedCategory === 'design' ? 'active' : ''} onClick={() => setSelectedCategory('design')}>Design</button>
           </div>
           <div className="projects_cards">
-            <Link href='/' className="procard">
-              <div className="proimgbox">
-                <img src="/img/climate_app.png" alt="" />
-              </div>
-              <div className="procontentbox">
-                <h2>Climate App Design</h2>
-                <GoArrowUpRight />
-              </div>
-            </Link>
-            <Link href='/' className="procard">
-              <div className="proimgbox">
-                <img src="/img/dermatology_clinic.jpeg" alt="" />
-              </div>
-              <div className="procontentbox">
-                <h2>Dr Safdarian Clinic</h2>
-                <GoArrowUpRight />
-              </div>
-            </Link>
+            {filteredProjects.map((project, index) => (
+              <Link key={index} href='/' className="procard">
+                <div className="proimgbox">
+                  <img src={project.image} alt={project.title} />
+                </div>
+                <div className="procontentbox">
+                  <h2>{project.title}</h2>
+                  <GoArrowUpRight />
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </section>
 
       {/* Experience study */}
       <section className="exstudy">
-
+        <div className="container flex flex-left flex-sb">
+          <div className="experience">
+            <div className="experience_title flex gap-1">
+              <LuMedal />
+              <h2>My Experience</h2>
+            </div>
+            <div className="exper_cards">
+              <div className="exper_card">
+                <span>Jun 2021 – Aug 2021</span>
+                <h2>Ant Coders</h2>
+                <p>Front-End Developer</p>
+              </div>
+              <div className="exper_card">
+                <span>Jan 2021 – Apr 2021</span>
+                <h2>Dexis Co</h2>
+                <p>Front-End Developer</p>
+              </div>
+              <div className="exper_card">
+                <span>Nov 2016 – Apr 2017</span>
+                <h2>TOD Co</h2>
+                <p>Back-End Developer Intern</p>
+              </div>
+            </div>
+          </div>
+          <div className="education">
+            <div className="experience_title flex gap-1">
+              <PiGraduationCap />
+              <h2>My Education</h2>
+            </div>
+            <div className="exper_cards">
+              <div className="exper_card">
+                <span>Sep 2024 - Present</span>
+                <h2>George Brown College, Toronto, Ontario</h2>
+                <p>Web Development (Front-End Design) | Ontario College Graduate Certificate</p>
+              </div>
+              <div className="exper_card">
+                <span>Jan 2021 – Apr 2021</span>
+                <h2>Amirkabir University of Technology</h2>
+                <p>Information Technology | Bachelor of Science</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       {/* My Skills */}
       <section className="myskills">
-
+        <div className="container">
+          <div className="myskills_title">
+            <h2>My Skills</h2>
+            <p>Here’s what I bring to the table—React, Next.js, Material UI, Figma, and more—to build smooth, responsive, and user-friendly web experiences.</p>
+          </div>
+          <div className="myskils_cards">
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/nextjs.svg" alt="nextJS" />
+              </div>
+              <p className="text-center">NextJS</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/reactjs.svg" alt="ReactJS" />
+              </div>
+              <p className="text-center">ReactJS</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/js.svg" alt="javascript" />
+              </div>
+              <p className="text-center">JavaScript</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/typescript.svg" alt="typescript" />
+              </div>
+              <p className="text-center">TypeScript</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/redux.svg" alt="redux" />
+              </div>
+              <p className="text-center">Redux</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/sql.svg" alt="SQL" />
+              </div>
+              <p className="text-center">SQL</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/github.svg" alt="github" />
+              </div>
+              <p className="text-center">GitHub</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/vscode.svg" alt="vs code" />
+              </div>
+              <p className="text-center">VS Code</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/postman.svg" alt="Postman" />
+              </div>
+              <p className="text-center">Postman</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/figma.svg" alt="Figma" />
+              </div>
+              <p className="text-center">Figma</p>
+            </div>
+            <div className="mys_card">
+              <div className="mys_inner">
+                <img src="/img/mui.svg" alt="Material UI" />
+              </div>
+              <p className="text-center">Material UI</p>
+            </div>
+          </div>
+        </div>
       </section>
 
       {/* Recent Blogs */}

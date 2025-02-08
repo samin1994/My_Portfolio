7b330793e2107d47a281738a43232685eb14d4f6
samin1994 <saminlabbaf1994@gmail.com>
2025-02-08 01:47:19 -0500

    fixed hero image I hope! 5 final

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+  images: {
+    optimizeImages: false,
+  }
 };
 
 export default nextConfig;

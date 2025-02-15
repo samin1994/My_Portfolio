6eac0edbaa8b0604e6c8388c659e6ccb4e538abf
samin1994 <saminlabbaf1994@gmail.com>
2025-02-15 17:48:14 -0500

    image fixed in project slug final

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import climate_app from "../../public/img/climate_app.png";
+import dr_safdarian from "../../public/img/dermatology_clinic.jpeg";
 
 export default function projectslug() {
 
@@ -12,7 +14,7 @@ export default function projectslug() {
             title: "Dr Safdarian Clinic",
             description: "I designed high-fidelity pages for Dr. Negin Safdarian's clinic using Figma, followed by developing a responsive website with Next.js and Material UI. The site is optimized for SEO with semantic HTML and server-side rendering (SSR), ensuring faster load times and enhanced search engine visibility.",
             link: "https://www.neginsafdarian.com",
-            image: "/img/dermatology_clinic.png",
+            image: dr_safdarian,
             category: "code",
             skills: ["Figma", "Next.js", "Material UI"],
             date: "April 2023",
@@ -22,7 +24,7 @@ export default function projectslug() {
             title: "Climate App Design",
             description: "For ObsidianPulse, I designed a minimalist, accessible interface in Figma, focusing on intuitive navigation and clear visual hierarchy. I created interactive prototypes to showcase key features while considering accessibility through color contrast and legible typography. This project highlights my skills in Figma design and prototyping, ensuring an engaging, user-friendly experience.",
             link: "https://example.com/project2",
-            image: "/img/climate_app.png",
+            image: climate_app,
             category: "design",
             skills: ["Figma", "Prototyping"],
             date: "December 2024",
